a6563ea66a3358b6112cd277a5584c6fe8ca40b7
Pedro Carvalho <pedrohenrique.cn07@gmail.com>
2025-04-01 16:49:51 -0300

    Add files via upload

--- Aula25Mar/Exercicios/meu-projeto/public/Scripts/script.js
+++ Aula25Mar/Exercicios/meu-projeto/public/Scripts/script.js
@@ -1,60 +1,164 @@
-const e = require("express");
-
+//Recebe o id do campo e a mensagem a ser exibida
+//Exibe uma mensagem de erro do campo correspondente
 function exibirErro(campo, mensagem) {
     document.getElementById(`error-${campo}`).textContent = mensagem;
 }
-
+//Remove todas as mensagem de erro do formulário
+//Chama a função exibirErro para limpar os erros dos campos
 function limparErros() {
     exibirErro('nome', '');
     exibirErro('email', '');
     exibirErro('telefone', '');
+    exibirErro('id', '');
 }
-
-function validarCampor(){
+//Valida os campos do formulário antes do envio
+//Retorna 'true' se todos os campos estiverem preenchidos corretamente e 'false' caso o contrário
+function validarCampor() {
     let nome = document.getElementById('nome').value.trim();
     let email = document.getElementById('email').value.trim();
     let telefone = document.getElementById('telefone').value.trim();
 
-    let valido = true;
-    limparErros();
-    
-    if(nome === '') {
+    let valido = true;//Variavel armazena se a validação esta correta
+    limparErros();//remove mensagem de erro antes de validar
+
+    if (nome === '') {
         exibirErro('nome', 'Nome é obrigatório.');
         valido = false;
-    } 
-    if(email === '') {
+    }
+    if (email === '') {
         exibirErro('email', 'Email é obrigatório.');
         valido = false;
-    } else if(!email.includes('@')) {
+    } else if (!email.includes('@')) {
         exibirErro('email', 'Email inválido.');
         valido = false;
     }
-    if(telefone === '') {
+    if (telefone === '') {
         exibirErro('telefone', 'Telefone é obrigatório.');
         valido = false;
     }
     return valido;
 }
 
-function cadastrar(){
-    if(validarCampor()){
-        alert('Cadastro realizado com sucesso!');
-        document.getElementById('formulario').reset();
-        limparErros();
-    }
-}
+//Função para cadastrar usuarios
+function cadastrar() {
+    let nome = document.getElementById('nome').value.trim();
+    let email = document.getElementById('email').value.trim();
+    let telefone = document.getElementById('telefone').value.trim();
 
-function atualizar(){
-    if(validarCampor()){
-        alert('Dados atualizados com sucesso!');
-        limparErros();
-    }
-
-}
-
-function excluir(){
-    document.getElementById('formulario').reset();
     limparErros();
-    alert('Cadastro excluído com sucesso!');
+    if (nome === '') {
+        exibirErro('nome', 'Nome é obrigatório.');
+        return;
+    }
+    if (email === '') {
+        exibirErro('email', 'Email é obrigatório.');
+        return;
+    } else if (!email.includes('@')) {
+        exibirErro('email', 'Email inválido.');
+        return;
+    }
+    if (telefone === '') {
+        exibirErro('telefone', 'Telefone é obrigatório.');
+        return;
+    }
+    fetch('/cadastrar', {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ nome, email, telefone })
+    })
+        .then(response => response.json())//Converte a resposta do servidor para json
+        .then(data => alert('Usuário cadastrado com sucesso!'))
+        .catch(error => console.error('Erro', error));
 
+    document.getElementById('formulario').reset();    
+}
+
+function atualizar() {
+    limparErros();
+
+    const id = document.getElementById('id').value.trim();
+    if (id === "") {
+        exibirErro('id','Id é obrigatório para atualizar um usuário!');
+        return;
+    } else if (id <= 0) {
+        exibirErro('id', 'Id inválido!')
+        return;
+    }
+
+    const nome = document.getElementById('nome').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const telefone = document.getElementById('telefone').value.trim();
+
+    if (nome === '') {
+        exibirErro('nome', 'Nome é obrigatório.');
+        return;
+    }
+    if (email === '') {
+        exibirErro('email', 'Email é obrigatório.');
+        return;
+    } else if (!email.includes('@')) {
+        exibirErro('email', 'Email inválido.');
+        return;
+    }
+    if (telefone === '') {
+        exibirErro('telefone', 'Telefone é obrigatório.');
+        return;
+    }
+
+    fetch(`/usuarios/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ nome, email, telefone })
+    })
+        .then(response => {
+            if (response.ok) {
+                alert('Usuário atualizado com sucesso!');
+            } else {
+                return response.json().then(data => {
+                    throw new Error(data.erro || 'Erro ao atualizar usuário.');
+                });
+            }
+        })
+        .catch(error => {
+            console.error('Erro: ' + error);
+        });
+    document.getElementById('formulario').reset();
+}
+
+
+function excluir() {
+    limparErros();
+    const id = document.getElementById('id').value.trim();
+    if (id === "") {
+        exibirErro('id', 'Id é necessário para deletar um usuário!')
+        return;
+    } else if (id <= 0) {
+        exibirErro('id', 'Id inválido!')
+        return;
+    }
+
+    fetch(`/usuarios/${id}`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(response => {
+            if (response.ok) {
+                alert('Usuário excluído com sucesso!');
+            } else {
+                return response.json().then(data => {
+                    throw new Error(data.erro || 'Erro ao excluir usuário.');
+                });
+            }
+
+        })
+        .catch(error => {
+            console.error('Erro: ' + error);
+        });
+    document.getElementById('formulario').reset();
 }
--- Aula25Mar/Exercicios/meu-projeto/server.js
+++ Aula25Mar/Exercicios/meu-projeto/server.js
@@ -1,17 +1,115 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
+const sqlite3 =  require('sqlite3').verbose();
+
 const app = express();
+const db = new sqlite3.Database('./usuarios.bd');
 
+app.use(express.json());
+app.use(cors());
+
+//Criar tabela de usuários se não existir
+db.run(`CREATE TABLE IF NOT EXISTS usuarios(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    telefone TEXT NOT NULL
+)`);
+
+//middleware para validar dados do usuário
+const validarUsuario = (req, res, next) => {
+    const {nome, email, telefone} = req.body;
+    if(!nome || !email || !telefone){
+        return res.status(400).json({erro: 'Todos os campos são obrigatorios'});
+    }
+    next();
+}
+
+//Define a pasta public como estatica para servir arquivos html, css e js
 app.use(express.static(path.join(__dirname, 'public')));
-
+//Rota principal para o link da pagina principal
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+//Rota para listar todos os usuários
+app.get('/usuarios', (req, res) =>{
+    db.all(`
+        SELECT id, nome, email, telefone from usuarios;`, //Rota do SQL para a seleção dos campos preenchidos
+        [], //Array vazio pois não há parametros dinamicos na consulta
+        (err, rows) =>{//Callback "Recebe o erro (se houver) e a linha retornada"
+            if(err)
+            return res.status(500).json({erro: err.message})//Retorna erro 500, caso tenha falha
+            res.json(rows);//Retorna os dados obtidos do banco em json
+        }
+    )
+});
+
+//Rota para cadastrar usuario
+app.post('/cadastrar', validarUsuario, (req, res) =>{
+    //Obtem os dados eviados no corpo da requisição
+    const {nome, email, telefone} = req.body;
+    //Insere um novo usuario na tablea usuarios
+    db.run(`
+        INSERT INTO usuarios (nome, email, telefone)
+        VALUES(?, ?, ?)`, 
+        [nome, email, telefone],// Valores que serão inseridos
+        function(err){//Se houver um erro, uma função é passada como argumento
+            if(err){
+                if(err.code === 'SQLITE_CONSTRAINT'){//Verifica uma violação
+                    return res.status(400).json({erro: 'Email já cadastrado'})
+                }
+                return res.status(500).json({erro: err.message})
+            }
+            res.json({id: this.lastId, mensagem:'Usuario cadastrado com sucesso'});
+        }
+    )
+});
+
+//Rota para deletar usuario
+app.delete('/usuarios/:id', (req, res) =>{
+    const {id} = req.params
+    db.run(`
+        DELETE FROM usuarios WHERE id = ?`,
+        [id],
+        function (err) {
+            if (err) {
+                return res.status(500).json({ erro: err.message });
+            }
+            if (this.changes === 0) {
+                return res.status(404).json({ erro: 'Usuário não encontrado' });
+            }
+            res.json({ mensagem: 'Usuário excluído com sucesso' });
+        }
+    )
+
+})
+
+//Rota para atualizar usuario
+app.put('/usuarios/:id', (req, res) => {
+    const {id} = req.params;
+    const {nome, email, telefone} = req.body;
+    db.run(`
+        UPDATE usuarios SET nome = ?, email = ?, telefone = ? WHERE id = ?`,
+        [nome, email, telefone, id],
+        function(err){
+            if(err){
+                return res.status(500).json({erro: err.message});
+            }
+            if(this.changes === 0){
+                return res.status(404).json({erro: 'Usuario não encontrado'});
+            }
+            res.json({mensagem: 'Usuario atualizado com sucesso'});
+        }
+    )
+})
+
+//Define a porta de IP
 const PORT = 3000;
-const HOST = '0.0.0.0';
+const HOST = '0.0.0.0';//Permite conexões de outros dispositivos na rede
 app.listen(PORT, () => {
     console.log(`Servidor rodando em: http://${HOST}:${PORT}`);
-    console.log(`Acesse via IP LOCAL: http://10.83.138.62:${PORT}`);
+    console.log(`Acesse via IP LOCAL: http://10.83.138.62:${PORT}`);//Substitue pelo ip realocado
     console.log('Para derrubar o servidor: ctrl + c');
 });
