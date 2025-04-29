4fcd4395f4dae3d7604499d639feabeb5806d4a3
Pedro Carvalho <pedrohenrique.cn07@gmail.com>
2025-04-29 17:15:59 -0300

    Add files via upload

---
+++ src/components/MeuComponente.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function MeuComponente() {
+    return (
+        <div>
+        <h1>Meu Componente</h1>
+        <p>Este é um componente de exemplo.</p>
+        <p>Pscininha amor, Pscininha amoor</p>
+        
+        </div>
+    );
+}
+
+export default MeuComponente;
